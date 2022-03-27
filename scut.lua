41311a118e5c83ac2fa7a63faccf972070dc833d
OGalloy <56289594+OGalloy@users.noreply.github.com>
2022-03-27 20:29:17 +0500

    Update scut.lua

--- scut.lua
+++ scut.lua
@@ -7,13 +7,18 @@ scut.execute = function(command)
 	return recieve
 end
 --Receive file info.
-scut.fileInfo = function(fileName)
-	local result = scut.execute("ffprobe -i "..fileName.." -hide_banner")
+scut.fileInfo = function(inputFile)
+	local result = scut.execute("ffprobe -i "..inputFile.." -hide_banner")
 	return result
 end
---Information about us.
+--Information about programm.
 scut.aboutUs = function()
 		print("This is programm for cuting video and audio")
 end
 
---scut.cutVideoFragment = function(
+scut.cutVideoFragment = function(inputFile, time1, time2)
+	local result = scut.execute("ffmpeg -i "..inputFile..
+								" -ss "..time1.." -t "..time2..
+								" -vcodec copy -acodec copy scut.mp4")
+	return result
+end
